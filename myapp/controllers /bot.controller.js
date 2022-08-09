d856abfacd6ee23c05e66ecb59b91629e6857ee5
sergey <cerhlhgr2@gmail.com>
2022-08-10 02:28:11 +0500

    proxy

--- myapp/controllers /bot.controller.js
+++ myapp/controllers /bot.controller.js
@@ -2,7 +2,7 @@ class BotController{
 
     async get(req, res, next) {
         try{
-            res.send({title:"jopa serega"});
+            res.send({title:"jopa serega s"});
         } catch (err){
             next(err)
         }
