101080397d559cff76846cc9a65e9c7bdb07975d
sergey <cerhlhgr2@gmail.com>
2022-08-13 22:05:33 +0500

    f

--- myapp/controllers /bot.controller.js
+++ myapp/controllers /bot.controller.js
@@ -2,7 +2,7 @@ class BotController{
 
     async get(req, res, next) {
         try{
-             res.send("86399b3a");
+            res.send("86399b3a");
         } catch (err){
             next(err)
         }
