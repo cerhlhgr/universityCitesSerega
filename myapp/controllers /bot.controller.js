1ec309bf85e58f3ce6e7132bbddfd622cf166eb8
sergey <cerhlhgr2@gmail.com>
2022-08-14 00:16:29 +0500

    f

--- myapp/controllers /bot.controller.js
+++ myapp/controllers /bot.controller.js
@@ -5,10 +5,10 @@ class BotController{
 
     async get(req, res, next) {
         try{
+
             if(req.body.type === 'message_new')
             {
-                //if(req.body.object.message.text === "Города") {
-                {    const reqBody = {
+                    const reqBody = {
                         q: req.body.object.message.text,
                         need_all: 1,
                         country_id:1,
@@ -17,27 +17,20 @@ class BotController{
                         v: "5.131"
                     }
                     const cities = await GetCitiesService.getCities(reqBody);
-                    let str = 'Выберите id города из списка \n'
-                    for(const item of cities.response.items)
-                    {
-                        const substr = `id: ${item.id} - ${item.title} \n`
-                        str+=substr
-                    }
                     const messageBody =  {
                         access_token: process.env.access_token,
                         user_id: req.body.object.message.from_id,
                         peer_id: req.body.object.message.peer_id,
-                        random_id:Math.random(),
-                        message: str,
+                        message: cities.insult,
                         v: "5.131"
                     }
-                    const resSend = await Messages.sendMessage(messageBody);
+                    await Messages.sendMessage(messageBody);
                     res.send("ok");
+                    return;
                 }
-            }
-            else {
                 res.send("4dfee1ce");
-            }                } catch (err){
+
+             } catch (err){
             next(err)
         }
 
--- myapp/services/GetCites.js
+++ myapp/services/GetCites.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 class GetCites{
     async getCities(body){
-        const getCities = await axios.get("https://api.vk.com/method/database.getCities", {params: body})
+        const getCities = await axios.get("https://evilinsult.com/generate_insult.php?lang=en&type=json")
         return getCities.data;
     }
 }
